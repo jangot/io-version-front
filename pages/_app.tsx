4583baf18535d0019067a943a6b22ea73f9397f1
jangot <pulin.pavel@gmail.com>
2022-12-26 15:19:12 +0100

    add top bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import createCache from '@emotion/cache';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import ResponsiveAppBar from '../component/top-bar';
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export function createEmotionCache() {
+  return createCache({ key: 'css', prepend: true });
+}
+const clientSideEmotionCache = createEmotionCache();
+
+
+export default function App(props: MyAppProps) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    const theme = createTheme({})
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ResponsiveAppBar />
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
